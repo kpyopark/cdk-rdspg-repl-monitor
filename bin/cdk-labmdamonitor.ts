2b9aeb6265a08b74c378d6ed08c4d4762b72fea1
toheavener01@gmail.com <toheavener01@gmail.com>
2020-07-07 11:45:45 +0900

    Modified js async call to execute right order.

--- bin/cdk-labmdamonitor.ts
+++ bin/cdk-labmdamonitor.ts
@@ -4,5 +4,10 @@ import * as cdk from '@aws-cdk/core';
 import { CdkLabmdamonitorStack } from '../lib/cdk-labmdamonitor-stack';
 
 const app = new cdk.App();
-new CdkLabmdamonitorStack(app, 'CdkLabmdamonitorStack');
+new CdkLabmdamonitorStack(app, "CdkLabmdamonitorStack", {
+  env: {
+    account: "431153255821",
+    region: "ap-northeast-2"
+  },
+});
 
--- lib/cdk-labmdamonitor-stack.ts
+++ lib/cdk-labmdamonitor-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as ddb from "@aws-cdk/aws-dynamodb";
 import * as sns from "@aws-cdk/aws-sns";
 import { Rule, Schedule } from "@aws-cdk/aws-events";
 import { LambdaFunction } from "@aws-cdk/aws-events-targets";
-import * as ssm from "@aws-cdk/aws-ssm";
+import * as iam from '@aws-cdk/aws-iam';
+import * as ssm from '@aws-cdk/aws-ssm';
+import * as sm from '@aws-cdk/aws-secretsmanager';
 import * as lambdajs from "@aws-cdk/aws-lambda-nodejs";
+import { print } from 'util';
 
 export interface CronLambdaProps {
   codeReference: lambda.S3Code;
@@ -28,7 +32,9 @@ export class CdkLabmdamonitorStack extends cdk.Stack {
     const uploadBucketName = this.node.tryGetContext("S3_UPLOAD_BUCKET");
     const vpcId = this.node.tryGetContext("VPC_ID");
     const databaseMasterNodeDns = this.node.tryGetContext("RDS_MASTER_DNS");
-    const smkeydbpass = this.node.tryGetContext("SM_DB_PASS");
+    const targetsubnets = this.node.tryGetContext("LAMBDA_SUBNETS");
+    const smkeyarn = this.node.tryGetContext("SM_DB_PASS_ARN");
+    const dbname = this.node.tryGetContext("DB_NAME");
     const ddbtablename = getValueWithDefault(this.node.tryGetContext("DDB_TABLE_NAME"), "pgmonitor");
     const snstopicname = getValueWithDefault(
       this.node.tryGetContext("SNS_TOPIC_NAME"),
@@ -43,11 +49,7 @@ export class CdkLabmdamonitorStack extends cdk.Stack {
     // create DDB for monitoring repository
     const replstatusdb = new ddb.Table(this, ddbtablename, {
       partitionKey: {
-        name: "connections",
-        type: ddb.AttributeType.STRING,
-      },
-      sortKey: {
-        name: "status",
+        name: "status_pk",
         type: ddb.AttributeType.STRING,
       },
       billingMode: ddb.BillingMode.PAY_PER_REQUEST,
@@ -60,29 +62,57 @@ export class CdkLabmdamonitorStack extends cdk.Stack {
       topicName: snstopicname
     });
 
+    // make IVpc reference using 'Vpc.fromLookup'
+    const lambdaVpc = ec2.Vpc.fromLookup(this, "vpc", {
+      vpcId: vpcId
+    });
+
+    // make isubnet array from the "targetsubnets" variable. 
+    const targetsubnetarr = targetsubnets.split(",").map(function(subnet:string){
+      return { subnetId: subnet.trim() };
+    });
+
     // create lambda function with resources.
     // and register lambda handler to cloudwatch scheduler
     const handler = new lambdajs.NodejsFunction(this, "RDSMonitorLambda", {
       runtime: lambda.Runtime.NODEJS_12_X, // So we can use async in widget.js
       entry: "resources/RdsPgMonitor.js",
-      externalModules: ["aws-sdk", "pg"],
-      handler: "RdsPgMonitor.main",
-      vpc: vpcId,
+      nodeModules: ["aws-sdk", "pg", "dns"],
+      externalModules: ["aws-sdk", "pg", "dns"],
+      handler: "main",
+      vpc: lambdaVpc,
+      vpcSubnets: {
+        subnets: targetsubnetarr,
+      },
+      // role: customRole,
       environment: {
         BUCKET: bucket.bucketName,
-        DDB_TABLE_NAME: ddbtablename,
+        DDB_TABLE_NAME: replstatusdb.tableName,
         MIN_REPL_COUNT: minReplicationCount,
         MAX_REPL_LAGTIME: maxReplicationLagTime,
         SNS_TOPIC_ARN: eventtopic.topicArn,
         RDS_MASTER_DNS: databaseMasterNodeDns,
-        SM_DB_PASS_KEY: smkeydbpass,
+        SM_DB_PASS_ARN: smkeyarn,
+        DB_NAME: dbname,
       },
     });
 
     // grant permissions of the resource to the lambda function.
     replstatusdb.grantReadWriteData(handler);
     eventtopic.grantPublish(handler);
-    bucket.grantReadWrite(handler); 
+    bucket.grantReadWrite(handler);
+
+    const dbpasssecret = sm.Secret.fromSecretArn(this, `dbpasskeysecret`, smkeyarn);
+
+    handler.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [dbpasssecret.secretArn],
+      actions: [
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:DescribeSecret",
+                "secretsmanager:ListSecretVersionIds"
+            ]
+    }));
 
     // make scheduler for triggering lambda function.
     new Rule(this, "MonitoringCronRule", {
